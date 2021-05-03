ec2d99f4d66e3e158b43ed859f9a41069d1e27d0
Eldar Khaitov <eldar.khaitov@gmail.com>
2021-05-03 23:37:43 +0300

    redesigned settings page

--- src/app/pages/user-settings/user-settings.module.ts
+++ src/app/pages/user-settings/user-settings.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserSettingsComponent } from './user-settings/user-settings.component';
-import {RouterModule, Routes} from "@angular/router";
-import {MatInputModule} from "@angular/material/input";
-import {MatSelectModule} from "@angular/material/select";
-import {MatCardModule} from "@angular/material/card";
-import {MatDividerModule} from "@angular/material/divider";
+import {RouterModule, Routes} from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 const routes: Routes = [
   {path: '', component: UserSettingsComponent}
@@ -21,7 +22,8 @@ const routes: Routes = [
     MatInputModule,
     MatSelectModule,
     MatCardModule,
-    MatDividerModule
+    MatDividerModule,
+    MatListModule
   ]
 })
 export class UserSettingsModule { }
