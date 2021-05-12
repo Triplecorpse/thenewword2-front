701a3581028b443c35a4c4393e932ac35cfc533f
Eldar Khaitov <Eldar.Khaitov@mheducation.com>
2021-05-12 22:20:55 +0300

    Showed word to learn on front

--- src/app/pages/exercise/exercise.module.ts
+++ src/app/pages/exercise/exercise.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExerciseComponent } from './exercise/exercise.component';
 import {RouterModule, Routes} from "@angular/router";
+import {MatCardModule} from "@angular/material/card";
 
 const routes: Routes = [
   { path: '', component: ExerciseComponent }
@@ -11,9 +12,10 @@ const routes: Routes = [
   declarations: [
     ExerciseComponent
   ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes)
-  ]
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        MatCardModule
+    ]
 })
 export class ExerciseModule { }
--- src/app/pages/exercise/exercise/exercise.component.ts
+++ src/app/pages/exercise/exercise/exercise.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {WordService} from "../../../services/word.service";
+import {IWord} from "../../../interfaces/IWord";
+import {take} from "rxjs/operators";
 
 @Component({
   selector: 'app-exercise',
@@ -7,11 +10,27 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./exercise.component.scss']
 })
 export class ExerciseComponent implements OnInit {
+  private wordsToLearn: IWord[] = [];
+  private lastAskedId = 0;
+  isLoaded: boolean;
 
-  constructor(private httpClient: HttpClient) { }
-
-  ngOnInit(): void {
-    this.httpClient.get('word/exercise').subscribe();
+  constructor(private wordService: WordService) {
   }
 
+  ngOnInit(): void {
+    this.wordService.getWordsToLearn()
+      .pipe(take(1))
+      .subscribe((words => {
+        this.isLoaded = true;
+        this.wordsToLearn = words;
+      }));
+  }
+
+  getWord(): IWord {
+    if (this.wordsToLearn[this.lastAskedId]) {
+      return this.wordsToLearn[this.lastAskedId];
+    }
+
+    return null;
+  }
 }
